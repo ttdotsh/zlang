0123c04e5e58137d74ae57906ed8942217c2a180
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-11-23 11:18:33 -0500

    add wip repl

---
+++ src/bin/repl/main.rs
@@ -0,0 +1,28 @@
+use genzlang::parse::stream::TokenStream;
+use std::io::{self, BufRead, Write};
+
+fn main() -> io::Result<()> {
+    let stdin_handle = io::stdin().lock();
+    let stdout_handle = io::stdout().lock();
+    start(stdin_handle, stdout_handle)
+}
+
+const PROMPT: &str = "🙏 -> ";
+
+fn start<R: BufRead, W: Write>(mut reader: R, mut writer: W) -> io::Result<()> {
+    loop {
+        write!(writer, "{}", PROMPT)?;
+        writer.flush()?;
+
+        let mut buffer = String::new();
+        reader.read_line(&mut buffer)?;
+
+        match buffer.as_str() {
+            "exit\n" => break,
+            source => {
+                TokenStream::new(&source).try_for_each(|t| writeln!(writer, "{:?}", t))?;
+            }
+        };
+    }
+    Ok(())
+}
