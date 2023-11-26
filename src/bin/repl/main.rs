ac5c3f57dd0c037e72925170f70cef6435ae9bc7
timTonelli <102175985+timTonelli@users.noreply.github.com>
2023-11-26 18:06:59 -0500

    remove prev implementation

--- src/bin/repl/main.rs
+++ src/bin/repl/main.rs
@@ -1,4 +1,6 @@
-use genzlang::parse::stream::TokenStream;
+#![allow(dead_code)]
+
+// use genzlang::parse::stream::TokenStream;
 use std::io::{self, BufRead, Write};
 
 fn main() -> io::Result<()> {
@@ -10,19 +12,19 @@ fn main() -> io::Result<()> {
 const PROMPT: &str = "🙏 -> ";
 
 fn start<R: BufRead, W: Write>(mut reader: R, mut writer: W) -> io::Result<()> {
-    loop {
-        write!(writer, "{}", PROMPT)?;
-        writer.flush()?;
-
-        let mut buffer = String::new();
-        reader.read_line(&mut buffer)?;
-
-        match buffer.as_str() {
-            "exit\n" => break,
-            source => {
-                TokenStream::new(&source).try_for_each(|t| writeln!(writer, "{:?}", t))?;
-            }
-        };
-    }
+    // loop {
+    //     write!(writer, "{}", PROMPT)?;
+    //     writer.flush()?;
+    //
+    //     let mut buffer = String::new();
+    //     reader.read_line(&mut buffer)?;
+    //
+    //     match buffer.as_str() {
+    //         "exit\n" => break,
+    //         source => {
+    //             TokenStream::new(&source).try_for_each(|t| writeln!(writer, "{:?}", t))?;
+    //         }
+    //     };
+    // }
     Ok(())
 }
